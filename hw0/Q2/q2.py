f42f338dd9040a7195ce4c51ab6c55e9d870bbf9
Tingyu Peng <tingyupeng@126.com>
2019-04-02 22:23:35 +0800

    add Q2

---
+++ hw0/Q2/q2.py
@@ -0,0 +1,12 @@
+#################################################
+#    >File Name: q2.py
+#    >Author: Tingyu Peng
+#    >mail: PengTingyu.d@gmail.com
+#    >Created Time: 2019年04月02日 星期二 21时47分15秒
+#################################################
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import PIL
+from PIL import ImageColor
+from PIL import Image
+print(type(ImageColor.getcolor('red','RGBA')))
