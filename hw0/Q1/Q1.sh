3910ab73270912ab108684547431cfc34af62cc2
Tingyu Peng <tingyupeng@126.com>
2019-04-02 22:18:56 +0800

    finish Q1

---
+++ hw0/Q1/Q1.sh
@@ -0,0 +1,8 @@
+####################
+# File Name: Q1.sh
+# Author: Tingyu Peng
+# mail: PengTingyu.d@gmail.com
+# Created Time: 2019年04月02日 星期二 21时05分56秒
+####################
+#!/bin/bash
+python q1.py
